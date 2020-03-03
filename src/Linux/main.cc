2a4dbea4edd0fa5e875eff63f633b990fbf5914f
BitTim <tim.anhalt.2004@protonmail.com>
2020-03-03 18:55:20 +0100

    Added Frame Limiting

--- src/Linux/main.cc
+++ src/Linux/main.cc
@@ -3,17 +3,28 @@
 
 #include "../lib/game.hh"
 
-Game* game;
+Game* game = nullptr;
+
+const int targetFPS = 60;
+const int targetFrameDelay = 1000 / targetFPS;
+Uint32 frameStart;
+int frameTime;
 
 int main()
 {
-  game = new Game("Bit Engine 2D", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640, 480, false);
+  game = new Game();
+  game->init("Bit Engine 2D", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640, 480, false);
 
-  while(Game::running)
+  while(game->isRunning())
   {
+    frameStart = SDL_GetTicks();
+
     game->handleEvents();
     game->update();
     game->render();
+
+    frameTime = SDL_GetTicks() - frameStart;
+    if(targetFrameDelay > frameTime) SDL_Delay(targetFrameDelay - frameTime);
   }
 
   game->terminate();
