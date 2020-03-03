a55673148161f9d444f34e27753b082555489d99
BitTim <tim.anhalt.2004@protonmail.com>
2020-03-03 18:25:04 +0100

    Moved most functions to Game Class to simplify main.cc

--- src/Linux/main.cc
+++ src/Linux/main.cc
@@ -1,55 +1,21 @@
 #include <iostream>
 #include <SDL2/SDL.h>
 
-SDL_Window* window;
-SDL_Renderer* renderer;
-SDL_Event event;
+#include "../lib/game.hh"
 
-bool running = true;
-
-void init()
-{
-  SDL_Init(SDL_INIT_EVERYTHING);
-  window = SDL_CreateWindow("Bit Engine 2D", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640, 480, 0);
-  renderer = SDL_CreateRenderer(window, -1, 0);
-}
-
-void terminate()
-{
-  SDL_DestroyRenderer(renderer);
-  SDL_DestroyWindow(window);
-  SDL_Quit();
-}
-
-void update()
-{
-  SDL_WaitEvent(&event);
-  if(event.type == SDL_QUIT) running = false;
-}
-
-void draw()
-{
-  SDL_SetRenderDrawColor(renderer, 0x00, 0x00, 0x00, 0xFF);
-  SDL_RenderClear(renderer);
-
-  SDL_Rect sample = {10, 10, 150, 150};
-  SDL_SetRenderDrawColor(renderer, 0xFF, 0x00, 0x00, 0xFF);
-  SDL_RenderFillRect(renderer, &sample);
-
-  SDL_RenderPresent(renderer);
-}
+Game* game;
 
 int main()
 {
-  init();
+  game = new Game("Bit Engine 2D", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640, 480, false);
 
-  while(running)
+  while(Game::running)
   {
-    update();
-    draw();
+    game->handleEvents();
+    game->update();
+    game->render();
   }
 
-  terminate;
-
+  game->terminate();
   return 0;
 }
